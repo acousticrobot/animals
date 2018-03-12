1eb87f533b3c343afe14696c48311a49ba1920ea
Jonathan Gabel <hello@jonathangabel.com>
2018-03-12 16:36:19 -0400

    basic assets

--- src/main.js
+++ src/main.js
@@ -6,14 +6,56 @@ const GameState = {
   // load the game assets before the game starts
   preload() {
     this.load.image('background', 'assets/images/background.png')
+    this.load.image('arrow', 'assets/images/arrow.png')
+    this.load.image('chicken', 'assets/images/chicken.png')
+    this.load.image('pig', 'assets/images/pig.png')
+    this.load.image('sheep', 'assets/images/sheep_128.png')
   },
 
   // executed after everything is loaded
   create() {
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
+    this.scale.pageAlignHorizontally = true
+    this.scale.pageAlignVertically = true
+
+
+    this.background = this.game.add.sprite(0, 0, 'background')
+
+    this.sheep = this.game.add.sprite(this.game.world.centerX, 230, 'sheep')
+    this.sheep.anchor.setTo(0.5)
+
+    //left arrow
+    this.leftArrow = this.game.add.sprite(80, 120, 'arrow');
+    this.leftArrow.anchor.setTo(0.5);
+    this.leftArrow.scale.x = -1;
+    this.leftArrow.customParams = {direction: -1};
+
+    //left arrow allow user input
+    this.leftArrow.inputEnabled = true;
+    this.leftArrow.input.pixelPerfectClick = true;
+    this.leftArrow.events.onInputDown.add(this.switchAnimal, this);
+
+    //right arrow
+    this.rightArrow = this.game.add.sprite(560, 120, 'arrow');
+    this.rightArrow.anchor.setTo(0.5);
+    this.rightArrow.customParams = {direction: 1};
+
+    //right arrow user input
+    this.rightArrow.inputEnabled = true;
+    this.rightArrow.input.pixelPerfectClick = true;
+    this.rightArrow.events.onInputDown.add(this.switchAnimal, this);
+
   },
 
   // this is executed multiple times per second
   update() {
+  },
+
+  switchAnimal(sprite, event) {
+    console.log('move animal')
+  },
+  animateAnimal(sprite, event) {
+    console.log('animate animal')
   }
 }
 
