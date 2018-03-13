62c84579c327efc3a30aed34e1485c7344ed9d8f
Jonathan Gabel <hello@jonathangabel.com>
2018-03-13 09:57:36 -0400

    add animations

--- src/main.js
+++ src/main.js
@@ -7,9 +7,10 @@ const GameState = {
   preload() {
     this.load.image('background', 'assets/images/background.png')
     this.load.image('arrow', 'assets/images/arrow.png')
-    this.load.image('chicken', 'assets/images/chicken.png')
-    this.load.image('pig', 'assets/images/pig.png')
-    this.load.image('sheep', 'assets/images/sheep.png')
+
+    this.load.spritesheet('rooster', 'assets/images/rooster_spritesheet.png', 128, 128, 4)
+    this.load.spritesheet('pig', 'assets/images/pig_spritesheet.png', 128, 128, 4)
+    this.load.spritesheet('sheep', 'assets/images/sheep_spritesheet.png', 128, 128, 4)
   },
 
   // executed after everything is loaded
@@ -21,8 +22,28 @@ const GameState = {
 
     this.background = this.game.add.sprite(0, 0, 'background')
 
-    this.sheep = this.game.add.sprite(this.game.world.centerX, 230, 'sheep')
-    this.sheep.anchor.setTo(0.5)
+    const animalData = [
+      { key: 'rooster', text: 'ROOSTER' },
+      { key: 'pig', text: 'PIG' },
+      { key: 'sheep', text: 'SHEEP' }
+    ]
+    this.animals = this.game.add.group()
+
+    let animal
+    // prepare each animal
+    animalData.forEach((element) => {
+      animal = this.animals.create(-1000, 230, element.key)
+      animal.customParams = { text: element.text }
+      animal.anchor.setTo(0.5)
+      animal.inputEnabled = true
+      animal.input.pixelPerfectClick = true
+      animal.animations.add('animate', [0, 1, 2, 2, 3, 0, 1, 2, 2, 3, 0], 10, false)
+      animal.events.onInputDown.add(this.eventAnimateAnimal, this)
+    })
+
+    //  place current animal in the middle
+    this.currentAnimal = this.animals.next()
+    this.currentAnimal.position.set(this.game.world.centerX, 230)
 
     // left arrow
     this.leftArrow = this.game.add.sprite(80, 120, 'arrow')
@@ -33,7 +54,7 @@ const GameState = {
     // left arrow allow user input
     this.leftArrow.inputEnabled = true
     this.leftArrow.input.pixelPerfectClick = true
-    this.leftArrow.events.onInputDown.add(this.switchAnimal, this)
+    this.leftArrow.events.onInputDown.add(this.eventSwitchAnimal, this)
 
     // right arrow
     this.rightArrow = this.game.add.sprite(560, 120, 'arrow')
@@ -43,20 +64,30 @@ const GameState = {
     // right arrow user input
     this.rightArrow.inputEnabled = true
     this.rightArrow.input.pixelPerfectClick = true
-    this.rightArrow.events.onInputDown.add(this.switchAnimal, this)
+    this.rightArrow.events.onInputDown.add(this.eventSwitchAnimal, this)
   },
 
   // this is executed multiple times per second
   update() {
   },
 
-  eventSwitchAnimal() {
-    // console.log('move animal')
-  }
+  eventSwitchAnimal(sprite) {
+    let newAnimal
 
-  // animateAnimal(sprite, event) {
-  //   console.log('animate animal')
-  // }
+    if (sprite.customParams.direction > 0) {
+      newAnimal = this.animals.next()
+    } else {
+      newAnimal = this.animals.previous()
+    }
+
+    this.currentAnimal.x = -1000
+    newAnimal.x = this.game.world.centerX
+    this.currentAnimal = newAnimal
+  },
+
+  eventAnimateAnimal(sprite) {
+    sprite.play('animate')
+  }
 }
 
 // initiate the Phaser framework
